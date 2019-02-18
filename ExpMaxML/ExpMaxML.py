ed0f6adc95a667cac8db2ea35f61373cc51c3952
Naresh Kumar <nareshkumar66675@gmail.com>
2019-02-18 15:32:01 -0600

    Functionality to run for multiple datasets

--- ExpMaxML/ExpMaxML.py
+++ ExpMaxML/ExpMaxML.py
@@ -7,14 +7,46 @@ import numpy as np
 import sys
 
 liverFilePath = r'C:\Users\kumar\Downloads\indian-liver-patient-records\indian_liver_patient.csv'
+wineFilePath = r"C:\Users\kumar\OneDrive\Documents\Projects\ExpMaxML\Dataset\winequality-red.csv"
+
 liverDF = pd.read_csv(liverFilePath)
+wineDF = pd.read_csv(wineFilePath)
+
 liverDF.replace('', np.nan, inplace=True)
 liverDF = liverDF.dropna()
-#dataArr=liverDF.iloc[:,8].values
+
+
+wineDF.replace('', np.nan, inplace=True)
+wineDF = wineDF.dropna()
 
 
 while True:
-    numCols = list(liverDF.select_dtypes([np.number]).columns.values)
+
+    
+    print('1.Liver')
+    print('2.Wine')
+    print('3.Custom(File Path Needed)')
+    dataChoice = int(input('Select one Dataset from above : '))
+
+    if dataChoice == 1:
+        selectedDF = liverDF
+    elif dataChoice == 2:
+         selectedDF = wineDF
+    elif dataChoice == 3:
+        customFilePath = str(input('Enter full file Path : '))
+        customDF = pd.read_csv(customFilePath)
+        customDF.replace('', np.nan, inplace=True)
+        customDF = liverDF.dropna()
+        selectedDF = customDF
+    else:
+        choice = input('Enter Valid Option. Press Y to Restart and N to Exit: ')
+        if str.lower(choice) == 'n':
+            sys.exit()
+        else:
+            continue
+
+
+    numCols = list(selectedDF.select_dtypes([np.number]).columns.values)
 
     print('Available Columns')
     for i in range(len(numCols)):
@@ -22,8 +54,15 @@ while True:
 
     colNo = input("Select one Column(Enter number): ")
 
-    dataArr=liverDF.iloc[:,liverDF.columns.get_loc(numCols[int(colNo)])].values
+    dataArr=selectedDF.iloc[:,selectedDF.columns.get_loc(numCols[int(colNo)])].values
     #stat = StatData.Stat([0,1,1.5],[1,0.5,0.2],[1/3,1/3,1/3])
+
+    print('Close the Graph to continue.')
+
+    plt.hist(dataArr, bins='auto')
+    plt.title("Histogram for Column : " +numCols[int(colNo)])
+    plt.plot()
+    plt.show()
 
     cluster = FindOptimalClusters(dataArr) #int(input("Enter number of clusters"))
     #cluster = int(input("Enter number of clusters"))
@@ -35,7 +74,7 @@ while True:
             continue
         
     else:
-        print('Optimal Cluster is'+str(cluster))
+        print('Optimal Cluster is '+str(cluster))
     
 
     stat = StatData.Stat()
@@ -44,7 +83,7 @@ while True:
     logs = []
     delts=[]
     for x in range(25):
-        print('Iteration '+str(x))
+        print('*******  Iteration '+str(x) +'   *********')
         rValue = EStep(dataArr,cluster,stat)
         stat = MStep(dataArr,cluster,stat,rValue)
         print('Printing Mean')
@@ -52,24 +91,27 @@ while True:
         #print('Printing Variance')
         #print(*stat.variance, sep = ", ")
         logLikely = LogLikelyHood(dataArr,stat)
-        print('Printing Log Likelyhood')
-        print(logLikely)
+        print('Log Likelyhood :'+ str(logLikely))
         logs.append(logLikely)
         if x!=0:
             delts.append(logs[x]-logs[x-1])
             print((delts[len(delts)-2]-delts[len(delts)-1])/delts[len(delts)-2])
             if len(delts)>1:
-                if delts[len(delts)-2] != 0 and ((delts[len(delts)-2]-delts[len(delts)-1])/delts[len(delts)-2]<.2):    
+                if delts[len(delts)-2] != 0 and ((delts[len(delts)-2]-delts[len(delts)-1])/delts[len(delts)-2]<.1):    
                     break
-
-    plt.title('Likelihood Convergence')
-    plt.plot(logs)
-    plt.ylabel('Likelihood')
-    plt.show()
 
     print('Final Mean Values for each cluster:')
     for i in range(len(stat.mean)):
         print('Cluster '+str(i)+"  : "+str(stat.mean[i]))
+
+    print('Close the Graph to continue.')
+
+    plt.title('Likelihood Convergence: ' + numCols[int(colNo)])
+    plt.plot(logs)
+    plt.ylabel('Likelihood')
+    plt.show()
+
+
 
     choice = input('Press Y to Restart and N to Exit: ')
 
