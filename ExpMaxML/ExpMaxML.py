5b575f81cf75df5fa2ec6bfdde3856b7ef3e5e14
Naresh Kumar <nareshkumar66675@gmail.com>
2019-02-18 16:09:50 -0600

    Added Comments for Main File

--- ExpMaxML/ExpMaxML.py
+++ ExpMaxML/ExpMaxML.py
@@ -8,9 +8,10 @@ import sys
 import os.path
 
 try:
+    #EM with GMM Algorithm
     print('Expectation Maximization using Gaussian Mixture Model')
 
-
+    #Read files from Dataset folder
     dataSetFolder = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
     liverFilePath = dataSetFolder+ os.path.join('\Dataset\indian_liver_patient.csv')
@@ -21,12 +22,10 @@ try:
 
     liverDF.replace('', np.nan, inplace=True)
     liverDF = liverDF.dropna()
-
-
     wineDF.replace('', np.nan, inplace=True)
     wineDF = wineDF.dropna()
 
-
+    #Repeat until user wants to exit
     while True:    
         print('1.Liver')
         print('2.Wine')
@@ -50,7 +49,7 @@ try:
             else:
                 continue
 
-
+        #Get Numeric Columns from Dataset
         numCols = list(selectedDF.select_dtypes([np.number]).columns.values)
 
         print('Available Columns')
@@ -59,32 +58,42 @@ try:
 
         colNo = input("Select one Column(Enter number): ")
 
+        #Retrieve Column data based on user input
         dataArr = selectedDF.iloc[:,selectedDF.columns.get_loc(numCols[int(colNo)])].values
         #stat = StatData.Stat([0,1,1.5],[1,0.5,0.2],[1/3,1/3,1/3])
 
         print('Close the Graph to continue.')
 
+        #Shows the Histogram of Data
         plt.hist(dataArr, bins='auto')
         plt.title("Histogram for Column : " + numCols[int(colNo)])
         plt.plot()
         plt.show()
 
+        # Finds Optimal Clusters based on Likelihood
         cluster = FindOptimalClusters(dataArr) #int(input("Enter number of clusters"))
-        #cluster = int(input("Enter number of clusters"))
+
         if cluster == 0:
             print('Cannot find Optimal number of Clusters')
             try:
                 cluster = int(input('Enter Cluster to continue(integer to continue string to restart:'))
             except (ValueError, TypeError):
-                continue
-        
+                continue      
         else:
             print('Optimal Cluster is ' + str(cluster))
     
-
+        # Initiliaze Stat Object - Contains mean,variance and pi
+        # Predict inital stat values based on dataset
         stat = StatData.Stat()
         stat.SetInitialValues(cluster,dataArr)
 
+
+        ''' Execute the Alogorithm 
+            1. Execute E Step - Calculates probability of each data for each cluster
+            2. Execute M Step - Calculate the weigh and update mean,stat and pi
+            3. Find Log likelihood
+            4. If Converges Exit, else Repeat
+        '''
         logs = []
         delts = []
         for x in range(25):
@@ -93,11 +102,16 @@ try:
             stat = MStep(dataArr,cluster,stat,rValue)
             print('Printing Mean')
             print(*stat.mean, sep = ", ")
-            #print('Printing Variance')
-            #print(*stat.variance, sep = ", ")
             logLikely = LogLikelyHood(dataArr,stat)
             print('Log Likelyhood :' + str(logLikely))
             logs.append(logLikely)
+
+            ''' To find if likelihood converges
+                Find the delta of each log likelihood values
+                For each delta, find the increase in value.
+                if it is less than 10% Stop, else Repeat
+            '''
+
             if x != 0:
                 delts.append(logs[x] - logs[x - 1])
                 print((delts[len(delts) - 2] - delts[len(delts) - 1]) / delts[len(delts) - 2])
@@ -115,8 +129,6 @@ try:
         plt.plot(logs)
         plt.ylabel('Likelihood')
         plt.show()
-
-
 
         choice = input('Press Y to Restart and N to Exit: ')
 
